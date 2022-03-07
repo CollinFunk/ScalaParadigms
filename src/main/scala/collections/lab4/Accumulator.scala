8a7e0850f0e7640dcdfdd372b05e26347e9f4983
Collin Funk <65689063+CollinFunk@users.noreply.github.com>
2022-03-06 23:17:09 -0800

    updated lab4

--- src/main/scala/collections/lab4/Accumulator.scala
+++ src/main/scala/collections/lab4/Accumulator.scala
@@ -1,11 +1,32 @@
 package collections.lab4
 
-trait instruction {
-  def impl(reg: Int): Int
-}
+
 
 object Accumulator extends App {
-  def accum(program: List[instruction]): Int =
+  def inc(r: Int): Int = r + 1
+  def add(n: Int): Int => Int =
+    _+n
 
+  def mul(n: Int): Int => Int =
+    _ * n
+
+  def clear(n: Int): Int => Int =
+    _ * 0
+
+  def display(n: Int): Int => Int =
+    println(_)
+    _ + 0
+
+
+  def accum(program: List[Int => Int]): Int =
+    var reg: Int = 0
+    for (i <- 0 to program.length - 1)
+      reg = program(i).apply(reg)
+      println(reg)
     1
+
+  val program =
+    List(add(3), mul(4), add(5), add(9), mul(2))
+
+  accum(program)
 }
